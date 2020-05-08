83e15cd8c6d675a572e9a0288e9938b06fb4ed56
afoukal <ana.foukal@gmail.com>
2020-05-08 10:15:06 -0500

    added variables.py

---
+++ variables.py
@@ -0,0 +1,12 @@
+string_1 = "ana"
+string_2 = '"ANA"'
+string_3 = 'Ana'
+
+print(string_1)
+print(string_2)
+print(string_3)
+
+result = input('How many?')
+result_1=int(result)
+print(result)
+type(result)
