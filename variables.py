50e157e09d4cae7a7f653d492a57629de6e74c79
afoukal <ana.foukal@gmail.com>
2020-05-08 10:33:18 -0500

    changed variables.py

--- variables.py
+++ variables.py
@@ -1,12 +1,20 @@
-string_1 = "ana"
-string_2 = '"ANA"'
-string_3 = 'Ana'
 
-print(string_1)
-print(string_2)
-print(string_3)
+poem = 'Roses are red, violets are blue..'
+string_10 = 'Roses'
+result_2 = string_10 in poem
+print(result_2)
 
-result = input('How many?')
-result_1=int(result)
+string_11 = 'mak'
+result_3 = string_11 not in poem
+print(result_3)
+
+print(string_10*5)
+print(len(poem))
+result_4 = poem.count(string_10)
+print(result_4)
+
+
+str_1 = "hello "
+str_2 = "world"
+result = str_1 + str_2
 print(result)
-type(result)
